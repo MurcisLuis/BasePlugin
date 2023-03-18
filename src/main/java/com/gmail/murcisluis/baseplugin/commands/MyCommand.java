13ed57257a24448e9f53b385aba66900aa952390
BuildTools <unconfigured@null.spigotmc.org>
2023-03-18 23:55:44 +0100

    Base test

--- src/main/java/com/gmail/murcisluis/baseplugin/commands/MyCommand.java
+++ src/main/java/com/gmail/murcisluis/baseplugin/commands/MyCommand.java
@@ -1,9 +1,21 @@
 package com.gmail.murcisluis.baseplugin.commands;
 
 import com.gmail.murcisluis.baseplugin.api.commands.AbstractCommand;
+import com.gmail.murcisluis.baseplugin.api.commands.CommandHandler;
+import com.gmail.murcisluis.baseplugin.api.commands.TabCompleteHandler;
 
 public class MyCommand extends AbstractCommand {
     public MyCommand() {
+        super("baseplugin");
     }
 
+    @Override
+    public CommandHandler getCommandHandler() {
+        return null;
+    }
+
+    @Override
+    public TabCompleteHandler getTabCompleteHandler() {
+        return null;
+    }
 }
