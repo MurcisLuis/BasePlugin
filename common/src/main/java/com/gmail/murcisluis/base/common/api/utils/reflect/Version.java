d43c466aab8d10c0e303cba0dfb4c59338882bf4
Murcis Luis <luis123aldea@gmail.com>
2025-06-23 01:03:03 +0200

    Fix syntax error in Version.java - Remove duplicate semicolons

--- common/src/main/java/com/gmail/murcisluis/base/common/api/utils/reflect/Version.java
+++ common/src/main/java/com/gmail/murcisluis/base/common/api/utils/reflect/Version.java
@@ -1,46 +1,46 @@
 package com.gmail.murcisluis.base.common.api.utils.reflect;
 
+import lombok.Getter;
 import lombok.NonNull;
+import org.bukkit.Bukkit;
 import org.jetbrains.annotations.Nullable;
 
 /**
  * Enum of supported NMS versions.
  */
+@Getter
 public enum Version {
-	v1_8_R1(8),
-	v1_8_R2(8),
-	v1_8_R3(8),
-	v1_9_R1(9),
-	v1_9_R2(9),
-	v1_10_R1(10),
-	v1_11_R1(11),
-	v1_12_R1(12),
-	v1_13_R1(13),
-	v1_13_R2(13),
-	v1_14_R1(14),
-	v1_15_R1(15),
-	v1_16_R1(16),
-	v1_16_R2(16),
-	v1_16_R3(16),
-	v1_17_R1(17),
-	v1_18_R1(18),
-	v1_18_R2(18),
-	v1_19_R1(19),
-	v1_19_R2(19),
-	v1_19_R3(19),
-	v1_19_R4(19),
-	v1_20_R1(20),
-	v1_20_R2(20),
-	v1_20_R3(20),
-	v1_20_R4(20),
-	v1_20_R5(20),
-	v1_20_R6(20),
-	v1_21_R1(21),
-	v1_21_R2(21),
-	v1_21_R3(21),
-	v1_21_R4(21),
-	v1_21_R5(21),
-	v1_21_R6(21),
+	v1_8_R1(8, "1.8"),
+	v1_8_R2(8, "1.8.3"),
+	v1_8_R3(8, "1.8.4", "1.8.5", "1.8.6", "1.8.7", "1.8.8"),
+	v1_9_R1(9, "1.9", "1.9.2"),
+	v1_9_R2(9, "1.9.4"),
+	v1_10_R1(10, "1.10", "1.10.2"),
+	v1_11_R1(11, "1.11", "1.11.1", "1.11.2"),
+	v1_12_R1(12, "1.12", "1.12.1", "1.12.2"),
+	v1_13_R1(13, "1.13"),
+	v1_13_R2(13, "1.13.1", "1.13.2"),
+	v1_14_R1(14, "1.14", "1.14.1", "1.14.2", "1.14.3", "1.14.4"),
+	v1_15_R1(15, "1.15", "1.15.1", "1.15.2"),
+	v1_16_R1(16, "1.16"),
+	v1_16_R2(16, "1.16.2", "1.16.3"),
+	v1_16_R3(16, "1.16.4", "1.16.5"),
+	v1_17_R1(17, "1.17", "1.17.1"),
+	v1_18_R1(18, "1.18", "1.18.1"),
+	v1_18_R2(18, "1.18.2"),
+	v1_19_R1(19, "1.19", "1.19.1", "1.19.2"),
+	v1_19_R2(19, "1.19.3"),
+	v1_19_R3(19, "1.19.4"),
+	v1_20_R1(20, "1.20", "1.20.1"),
+	v1_20_R2(20, "1.20.2"),
+	v1_20_R3(20, "1.20.3", "1.20.4"),
+	v1_20_R4(20, "1.20.5", "1.20.6"),
+    v1_21_R1(21, "1.21", "1.21.1"),
+    v1_21_R2(21, "1.21.2", "1.21.3"),
+    v1_21_R3(21, "1.21.4"),
+    v1_21_R4(21, "1.21.5"),
+    v1_21_R5(21, "1.21.6"),
+	;
 	;
 
 	/*
@@ -48,9 +48,33 @@ public enum Version {
 	 */
 
 	public static final Version CURRENT;
+	public static final String CURRENT_MINECRAFT_VERSION;
+
 
 	static {
-		CURRENT = Version.fromString(ReflectionUtil.getVersion());
+		CURRENT_MINECRAFT_VERSION = getCurrentMinecraftVersion();
+		CURRENT = getCurrentVersion();
+	}
+
+	private static String getCurrentMinecraftVersion() {
+		// Bukkit version (e.g., 1.20.6-R0.1-SNAPSHOT)
+		String bukkitVersion = Bukkit.getServer().getBukkitVersion();
+		// Minecraft version (e.g., 1.20.6)
+		return bukkitVersion.split("-", 2)[0];
+	}
+	private static Version getCurrentVersion() {
+		return fromMinecraftVersion(CURRENT_MINECRAFT_VERSION);
+	}
+	@Nullable
+	public static Version fromMinecraftVersion(String minecraftVersion) {
+		for (Version version : Version.values()) {
+			for (String candidateMinecraftVersion : version.getMinecraftVersions()) {
+				if (candidateMinecraftVersion.equals(minecraftVersion)) {
+					return version;
+				}
+			}
+		}
+		return null;
 	}
 
 	/**
@@ -122,13 +146,12 @@ public enum Version {
 	 */
 
 	private final int minor;
+	private final String[] minecraftVersions;
 
-	Version(int minor) {
+
+	Version(int minor, String... minecraftVersions) {
 		this.minor = minor;
-	}
-
-	public int getMinor() {
-		return minor;
+		this.minecraftVersions = minecraftVersions;
 	}
 
 }
